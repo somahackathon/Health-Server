a67e456d5bc281a5940193d848298f0c15ab05ea
hej090224 <fc49854985@gmail.com>
2026-07-18 15:28:48 +0900

    init

---
+++ src/test/java/team/soma/teto/health/HealthApplicationTests.java
@@ -0,0 +1,13 @@
+package team.soma.teto.health;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
